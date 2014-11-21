8a0feab7db628dddd54ebcb41c483fde37719f36
rysep2 <rysep2@bfh.ch>
2014-11-21 11:36:54 +0100

    one more

--- html/classes/Product.php
+++ html/classes/Product.php
@@ -12,7 +12,7 @@ class Product {
 		$_db = DBHandler::getInstance ();
 		$res = $_db->getAllProducts ();
 		
-		//loop through products and create url to product
+		//loop through products and create url link to product
 		while ( $products = $res->fetch_object () ) {
 			$url = $_SERVER ['PHP_SELF'];
 			$url = $url . "?id=" . $products->product_id;
